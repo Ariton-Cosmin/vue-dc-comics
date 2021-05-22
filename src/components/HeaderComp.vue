063b05e9275fa09e9bec1b53286744cee06dcb72
Cosmin-Ariton <cosminiris12@gmail.com>
2021-05-22 11:26:05 +0200

    header-second

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -3,16 +3,9 @@
     <img src="../assets/img/dc-logo.png" alt="dc-logo" />
 
     <ul class="text-uppercase">
-      <li>characters</li>
-      <li class="active">comics</li>
-      <li>movies</li>
-      <li>tv</li>
-      <li>games</li>
-      <li>collectibles</li>
-      <li>videos</li>
-      <li>fans</li>
-      <li>news</li>
-      <li>shop</li>
+      <li v-for="(link, index) in links" :key="index">
+        <p :class="{ active: link.current }">{{ link.text }}</p>
+      </li>
     </ul>
   </header>
 </template>
@@ -20,35 +13,91 @@
 <script>
 export default {
   name: "HeaderComp",
+
+  data() {
+    return {
+      links: [
+        {
+          text: "characters",
+          current: false,
+        },
+        {
+          text: "comics",
+          current: true,
+        },
+        {
+          text: "movies",
+          current: false,
+        },
+        {
+          text: "tv",
+          current: false,
+        },
+        {
+          text: "games",
+          current: false,
+        },
+        {
+          text: "collectibles",
+          current: false,
+        },
+        {
+          text: "videos",
+          current: false,
+        },
+        {
+          text: "fans",
+          current: false,
+        },
+        {
+          text: "news",
+          current: false,
+        },
+        {
+          text: "shop",
+          current: false,
+        },
+      ],
+    };
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 header {
-  display: flex;
-  justify-content: space-between;
+  position: relative;
   align-items: center;
+  text-align: center;
   width: 100%;
   height: 90px;
   padding: 0 10%;
 
   img {
     width: 65px;
+    position: absolute;
+    top: 12px;
+    left: 8rem;
   }
 
   ul {
     display: inline-block;
     li {
       display: inline-block;
-      margin: 0 12px;
-      padding: 36px 0;
-      font-size: 11px;
-      cursor: pointer;
-    }
+      &:first-child {
+        margin-left: 480px;
+      }
 
-    .active {
-      color: rgb(2, 130, 249);
-      border-bottom: 4px solid rgb(2, 130, 249);
+      p {
+        margin: 40px 12px;
+        padding-bottom: 33px;
+        font-size: 11px;
+        cursor: pointer;
+        &:hover,
+        &:active {
+          color: rgb(2, 130, 249);
+          border-bottom: 4px solid rgb(2, 130, 249);
+        }
+      }
     }
   }
 }
